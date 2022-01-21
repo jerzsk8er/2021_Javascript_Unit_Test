496ef0f99c64277e6e8403e16bc4af304b795ac4
jerzsk8er <jayden.cabrera@vtsd.com>
2022-01-21 07:58:08 -0500

    beep

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,6 +1,11 @@
 /* Put code for the task below */
-function functionName(){
-	alert("Run My Stuff");
-	
-	
+function functionName() {
+
+	document.getElementById("buttonTask1").style.display="block";
 }
+
+function imgdisappear(){
+
+	document.getElementById("imageTask1").style.display="none"
+}
+
--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,1 +1,13 @@
 /* Put code for the task below */
+
+function button1(){
+    document.getElementById("colortable").style.backgroundColor="Blue";
+}
+
+function button2(){
+    document.getElementById("colortable").style.backgroundColor="Purple";
+}
+
+function button3(){
+    document.getElementById("colortable").style.backgroundColor="Red";
+}
