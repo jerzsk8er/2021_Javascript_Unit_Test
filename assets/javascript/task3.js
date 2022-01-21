f0df829511646f878533382c8afde39397dc2504
Jayden <jayden.cabrera@vtsd.com>
2022-01-21 12:08:26 -0500

    beep

--- assets/javascript/task3.js
+++ assets/javascript/task3.js
@@ -1,5 +1,9 @@
 /* Put code for the task below */
 
 function paragraph(){
+    document.getElementById("paragraph").style.display="none";
+}
 
+function clickme(){
+    document.getElementById("clickchange").style.display="block";
 }
