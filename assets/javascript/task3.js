c482ab9bcdca6595d764dadf4f4dbcae84f249e0
jerzsk8er <jayden.cabrera@vtsd.com>
2022-01-24 09:41:08 -0500

    beep

--- assets/javascript/task3.js
+++ assets/javascript/task3.js
@@ -2,8 +2,5 @@
 
 function paragraph(){
     document.getElementById("paragraph").style.display="none";
-}
-
-function clickme(){
     document.getElementById("clickchange").style.display="block";
 }
